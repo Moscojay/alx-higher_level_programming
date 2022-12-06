843609c0b8eea510730047f0cc00e50ee45d3190
Salaudeen moshood <Olajuwonmoshood465@gmail.com>
2022-12-06 08:48:20 -0800

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,8 +4,8 @@
 def divisible_by_2(my_list=[]):
     copy = my_list.copy()
     for a in range(len(copy)):
-        if copy[a] % 2 = 0:
+        if copy[a] % 2 == 0:
             copy[a] = True
         else:
             copy[a] = False
-    return 
+    return copy
